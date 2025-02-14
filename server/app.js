cad929af6ce7146f61411e00d2b27f210e1054ca
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-02-14 22:23:41 +0530

    db connected

--- server/app.js
+++ server/app.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
 require("dotenv").config();
 
+const connectDB = require("./config/db");
+
 const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`server running at port : ${PORT}`);
 });
