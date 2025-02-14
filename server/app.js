baccf355f11f86526f74f84254faa7bd9720fbaa
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-02-14 23:50:16 +0530

    feat: user registration and login

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 require("dotenv").config();
 
 const connectDB = require("./config/db");
+const indexRoute = require("./routes/index");
 
 const app = express();
 
@@ -12,6 +13,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+
+//endpoint
+app.use("/api/v1", indexRoute);
 
 const PORT = process.env.PORT || 8000;
 
--- server/models/user-model.js
+++ server/models/user-model.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
@@ -12,6 +14,19 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 
