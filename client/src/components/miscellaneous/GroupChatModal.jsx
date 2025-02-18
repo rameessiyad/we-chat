f39f218c3f24f5b8ecb387b56ae23be70cfb6aca
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-02-18 15:36:30 +0530

    group modal updated

--- client/src/components/miscellaneous/GroupChatModal.jsx
+++ client/src/components/miscellaneous/GroupChatModal.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/prop-types */
 import {
   Button,
+  FormControl,
+  Input,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -9,10 +11,25 @@ import {
   ModalHeader,
   ModalOverlay,
   useDisclosure,
+  useToast,
 } from "@chakra-ui/react";
+import { useState } from "react";
+import { ChatState } from "../../context/ChatProvider";
 
 const GroupChatModal = ({ children }) => {
   const { isOpen, onClose, onOpen } = useDisclosure();
+
+  const [groupChatName, setGroupChatName] = useState();
+  const [selectedUsers, setSelectedUsers] = useState([]);
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const toast = useToast();
+
+  const { user, chats, setChats } = ChatState();
+
+  const handleSearch = async (query) => {};
   return (
     <>
       <span onClick={onOpen}>{children}</span>
@@ -20,9 +37,36 @@ const GroupChatModal = ({ children }) => {
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+          <ModalHeader
+            fontSize="35px"
+            fontFamily="Work sans"
+            display="flex"
+            justifyContent="center"
+          >
+            Create Group Chat
+          </ModalHeader>
           <ModalCloseButton />
-          <ModalBody></ModalBody>
+          <ModalBody display="flex" flexDir="column" alignItems="center">
+            {/* group name */}
+            <FormControl>
+              <Input
+                placeholder="Chat Name"
+                mb={3}
+                onChange={(e) => setGroupChatName(e.target.value)}
+              />
+            </FormControl>
+
+            {/* Search users*/}
+            <FormControl>
+              <Input
+                placeholder="Add users"
+                mb={1}
+                onChange={(e) => handleSearch(e.target.value)}
+              />
+            </FormControl>
+            {/* selected users */}
+            {/* render searched users */}
+          </ModalBody>
 
           <ModalFooter>
             <Button colorScheme="blue" mr={3} onClick={onClose}>
